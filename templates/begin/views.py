077fabe511398b629c5eb0eddf321adaa0357552
beegeiger <beegeiger@gmail.com>
2019-12-10 18:08:42 -0800

    Deleted extraneous Auth0 code

--- templates/begin/views.py
+++ templates/begin/views.py
@@ -10,45 +10,7 @@ app = Flask(__name__,
 
 hello_blueprint = Blueprint('hello',__name__)
 
-#####################################################@
 
-oauth = OAuth(app)
-
-# def slack_compliance_fix(session):
-#     def _fix(resp):
-#         token = resp.json()
-#         # slack returns no token_type
-#         token['token_type'] = 'Bearer'
-#         resp._content = to_unicode(json.dumps(token)).encode('utf-8')
-#         return resp
-#     session.register_compliance_hook('access_token_response', _fix)
-
-auth0 = oauth.register(
-    'auth0',
-    client_id=oauth_client_id,
-    client_secret=oauth_client_secret,
-    api_base_url='https://dev-54k5g1jc.auth0.com',
-    access_token_url='https://dev-54k5g1jc.auth0.com/oauth/token',
-    authorize_url='https://dev-54k5g1jc.auth0.com/authorize',
-    # compliance_fix=slack_compliance_fix,
-    # client_kwargs={
-    #     'scope': 'openid profile email',
-    # }
-)
-################################################################
-
-def requires_auth(f):
-    """Creats Decorator from AuthO to only allow logged-in users access to 
-    certain paths/routes within the application"""
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if 'profile' not in session:
-            # Redirect to Login page here
-            return redirect('/')
-        return f(*args, **kwargs)
-    return decorated
-
-#################################################################
 
 @hello_blueprint.route('/')
 
