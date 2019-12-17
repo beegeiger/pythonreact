815c7127df66d30eae0af1c8e0e3d58ef3a2c4e6
beegeiger <beegeiger@gmail.com>
2019-12-16 16:10:58 -0800

    Created new React App

--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../../App";
-import { Auth0Provider } from "../../react-auth0-spa";
+// import App from "../../App";
+// import { Auth0Provider } from "../../react-auth0-spa";
+import { Auth0Provider } from 'simple-auth0-react';
 import config from "./auth_config";
 import history from "./history";
+
+// if ngro gets rowdy with webpack, fix this in node_modules/react-dev-utils/webpackHotDevClient.js
+// https://github.com/facebook/create-react-app/blob/master/packages/react-dev-utils/webpackHotDevClient.js#L62
 
 
 // A function that routes the user to the right place
--- templates/static/js/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import routes from "./routes";
-
-
-
-ReactDOM.render(<p>Test 123</p>, document.getElementById("content"));
