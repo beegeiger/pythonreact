89e08a5bcfb359efaf152fe265ef609e0e2d13c8
beegeiger <beegeiger@gmail.com>
2019-12-17 18:10:55 -0800

    The Menu now actually drops down

--- newreact/src/Menu.js
+++ newreact/src/Menu.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useAuth0 } from 'simple-auth0-react';
-// import './semantic/dist/semantic.min.css';
-import './semantic/dist/semantic.css';
+
 
 export const Menu = props => {
     const { logout } = useAuth0()
     return (
         <>
-        <div class="ui icon button" data-content="Add users to your feed">
-  <i class="add icon"></i>
-</div>
+        <div class="ui compact menu">
+        <div class="ui simple dropdown item">
+            Dropdown
+            <i class="dropdown icon"></i>
+            <div class="menu">
+            <div class="item">Choice 1</div>
+            <div class="item">Choice 2</div>
+            <div class="item">Choice 3</div>
+            </div>
+        </div>
+        </div>
         </>
     );
 };
