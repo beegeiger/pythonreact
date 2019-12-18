110620182c2b92e8ad0e3484708481bf14907171
beegeiger <beegeiger@gmail.com>
2019-12-17 18:33:14 -0800

    Added Contact component

---
+++ newreact/src/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Contact = props => {
+    return (
+        <div class="ui cards">
+            <div class="card">
+                <div class="content">
+                    <div class="header">John Doe</div>
+                        <div class="description">
+                            Elliot Fu is a film-maker from New York.
+                        </div>
+                    </div>
+                <div class="ui bottom attached button">
+                <i class="add icon"></i>
+                Edit Contact
+                </div>
+            </div>
+        </div>
+    );
+};
