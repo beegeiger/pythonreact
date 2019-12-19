d5c00372aaa70da1e55333c7e64cbb1a2dd06735
beegeiger <beegeiger@gmail.com>
2019-12-18 21:20:19 -0800

    Building Contact

--- newreact/src/Contact.js
+++ newreact/src/Contact.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 export const Contact = props => {
+    const name = props.name
+    const email = props.email
+    const phone = props.phone
     return (
         <div class="ui cards">
             <div class="card">
                 <div class="content">
-                    <div class="header">John Doe</div>
+                    <div class="header">Name: { name } </div>
                         <div class="description">
-                            Elliot Fu is a film-maker from New York.
+                            Phone: { phone }
+                        </div>
+                        <div class="description">
+                            E-mail: { email }
                         </div>
                     </div>
                 <div class="ui bottom attached button">
                 <i class="add icon"></i>
                 Edit Contact
+                </div>
+                <div class="ui bottom attached button">
+                <i class="add icon"></i>
+                Delete Contact
                 </div>
             </div>
         </div>
