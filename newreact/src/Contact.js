8946db831655aaa809095c4236e13d15a726f3de
beegeiger <beegeiger@gmail.com>
2019-12-24 18:51:17 -0800

    Set up ContactContainer

--- newreact/src/Contact.js
+++ newreact/src/Contact.js
@@ -1,30 +1,27 @@
 import React from 'react';
 
+
 export const Contact = props => {
     const name = props.name
     const email = props.email
     const phone = props.phone
     return (
-        <div class="ui cards">
-            <div class="card">
-                <div class="content">
-                    <div class="header">Name: { name } </div>
-                        <div class="description">
-                            Phone: { phone }
-                        </div>
-                        <div class="description">
-                            E-mail: { email }
-                        </div>
-                    </div>
-                <div class="ui bottom attached button">
-                <i class="setting basic icon"></i>
-                Edit Contact
-                </div>
-                <div class="ui bottom attached button">
-                <i class="cancel circle basic icon"></i>
-                Delete Contact
-                </div>
+        <>
+        <div class="content">
+            <div class="header">
+                Name: { name } 
+            </div>
+            <div class="description">
+                Phone: { phone }
+            </div>
+            <div class="description">
+                E-mail: { email }
             </div>
         </div>
+        <div class="ui bottom attached button">
+        <i class="setting basic icon"></i>
+        Edit Contact
+        </div>
+        </>
     );
 };
--- newreact/src/ContactForm.js
+++ newreact/src/ContactForm.js
@@ -1,51 +1,38 @@
 import React from 'react';
 
 export const ContactForm = props => {
-    let name = ''
-    let email = ''
-    let phone = ''
-
-    if (typeof props.name !== 'undefined' || props.name.length > 0) {
-        name = props.name
-    }
-    if (typeof props.email !== 'undefined' || props.email.length > 0) {
-        email = props.email
-    }
-    if (typeof props.phone !== 'undefined' || props.phone.length > 0) {
-        phone = props.phone
-    }
-
+    const name = props.name
+    const email = props.email
+    const phone = props.phone
     return (
-        <div class="ui cards">
-            <div class="card">
-                <div class="content">
-                    <form class="ui form">
-                        <div class="header">
-                            <div class="field">
-                                <input type="text" name="name" placeholder="Contact Name" value={ name } />
-                            </div>
-                        </div>
-                        <div class="description">
-                            <div class="field">
-                                <input type="text" name="email" placeholder="Contact E-mail" value={ email } />
-                            </div>
-                        </div>
-                        <div class="description">
-                            <div class="field">
-                                <input type="text" name="phone" placeholder="Contact Phone #" value={ phone } />
-                            </div>
-                        </div>
-                    </form>
+        <>
+        <div class="content">
+            <form class="ui form">
+                <div class="header">
+                    <div class="field">
+                        <input type="text" name="name" placeholder="Contact Name" value={ name } />
+                    </div>
                 </div>
-                <div class="ui bottom attached button">
-                    <i class="setting basic icon"></i>
-                    Save Contact
+                <div class="description">
+                    <div class="field">
+                        <input type="text" name="email" placeholder="Contact E-mail" value={ email } />
+                    </div>
                 </div>
-                <div class="ui bottom attached button">
-                    <i class="cancel circle basic icon"></i>
-                    Delete Contact
+                <div class="description">
+                    <div class="field">
+                        <input type="text" name="phone" placeholder="Contact Phone #" value={ phone } />
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
+        <div class="ui bottom attached button">
+            <i class="setting basic icon"></i>
+            Save Contact
+        </div>
+        <div class="ui bottom attached button">
+            <i class="cancel circle basic icon"></i>
+            Delete Contact
+        </div>  
+        </>
     );
 };
