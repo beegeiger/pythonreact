7426fa15c5c7843abeee5ec59e54e8b2edd7a43d
beegeiger <beegeiger@gmail.com>
2020-01-16 15:36:25 -0800

    Added Server Call on Login

--- newreact/src/App.js
+++ newreact/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './semantic/dist/semantic.min.css';
 import {Auth0Provider, useAuth0, onRedirectCallback } from 'simple-auth0-react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -6,28 +6,51 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ContactsHandler } from './components/contacts/ContactsHandler';
 
 const Content = () => {
-  const { loginWithRedirect, user, logout } = useAuth0()
-  return (
-    <div className="App">
-
-      <ContactsHandler />
-      {user && <div>
-        <div onClick={() => logout()}>Logout</div>
-      </div>}
-      {!user && <div> 
-        <a className="App-link"
-          href="javascript:void(0)"
-          onClick={() => { loginWithRedirect()}} >
-          Login
-        </a>
-      </div>}
-      <header className="App-header">
-      {/* <pre><code>{JSON.stringify({user}, null, '  ')}</code></pre> */}
-       
-        
-      </header>
-    </div>
-  );
+    const { loginWithRedirect, user, logout } = useAuth0()
+    useEffect(() => {    
+        async function loginServer() {
+            fetch(('http://127.0.0.1:5000/login'), {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({user}, null, '  '),
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+                .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+        if ({user}) {
+            loginServer();
+            console.log('Server Login Called')
+        }
+        else {
+            console.log('Server Login Not Called because not logged in')
+        }
+    }, []);
+    return (
+        <div className="App">
+            <ContactsHandler />
+            {user && <div>
+                <div>{JSON.stringify({user}, null, '  ')}</div>
+                <div onClick={() => logout()}>Logout</div>
+            </div>}
+            {!user && <div> 
+                <h1>BeSafe Homepage</h1>
+                <a className="App-link"
+                  href="javascript:void(0)"
+                  onClick={() => { loginWithRedirect()}} >
+                  Login
+                </a>
+            </div>}
+            <header className="App-header">            
+            </header>
+        </div>
+    );
 }
 
 export const App = () => {
