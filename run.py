d5538bb59311080902119eed06b37cc614c45320
beegeiger <beegeiger@gmail.com>
2020-01-19 16:45:11 -0800

    Login Server Path Now Functional

--- run.py
+++ run.py
@@ -69,35 +69,29 @@ def callback_handling():
 def login_handling():
     # Handles response from token endpoint
 
-    print('data', request.data)
-    print('files', request.files)
-    print('values', request.values)
-    print('json', request.json)
-    print('jsontype', type(request.json))
-    print('json2', request.json['user']['given_name'])
-    fname = request.json['user']['given_name']
-    lname = request.json['user']['family_name']
+    name = request.json['user']['name']
+    username = request.json['user']['nickname']
     email = request.json['user']['email']
     auth0_id = request.json['user']['sub']
    
-
-
     #Sets the 'current_user' value in the session to the user's e-mail
-    # session['current_user'] = userinfo['email']
+    session['current_user'] = userinfo['auth0_id']
 
-    # #User Table is Queried to see if User already exists in dB
-    # user = User.query.filter_by(email=userinfo['email']).all()
+    #User Table is Queried to see if User already exists in dB
+    user = User.query.filter_by(auth0_id=auth0_id).all()
     
-    # #If the user isn't in the dBase, they are added
-    # if user == []:
-    #     new_user = User(name=userinfo['name'], email=userinfo['email'], username=userinfo['nickname'], fname=userinfo['given_name'], lname=userinfo['family_name'], created_at=datetime.datetime.now())
-    #     db.session.add(new_user)
+    #If the user isn't in the dBase, they are added
+    if user == []:
+        new_user = User(name=name, email=email, username=username, auth0_id=auth0_id, created_at=datetime.datetime.now())
+        db.session.add(new_user)
+        db.session.commit()
+        return "New User Success"
     
     # #The dBase changes are committed
-    # db.session.commit()
+    db.session.commit()
 
     #Redirects to the User Profile
-    return "Success"
+    return "Returning User Success"
 
 @app.route("/contacts")
 def user_contacts():
